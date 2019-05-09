2529846e6ee9cab8972993a32f22eb704641765c
atarus30 <39539658+atarus30@users.noreply.github.com>
2019-05-09 18:42:28 -0400

    Update Hangman.java

--- Hangman-Game/src/Hangman.java
+++ Hangman-Game/src/Hangman.java
@@ -1,9 +1,20 @@
-
+package game;
+import java.util.Scanner;
 public class Hangman {
-
+	public void showMenu() {
+		int option;
+		Scanner sc = new Scanner(System.in);
+		System.out.printIn("-------Menu-------");
+		System.out.printIn("1. Play");
+		System.out.printIn("2. Instructions");
+		System.out.printIn("3. Exit");
+		System.out.printIn("\nChoose the Option: ");
+		option = sc.nextInt();
+	}	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			Hangman hg = new Hangman();
+			hg.showMenu();
 	}
 
 }
